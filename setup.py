3c04bbda4055c2f2047c6698ca7d435357829c7c
One <imoneoi@users.noreply.github.com>
2024-01-25 01:10:47 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,15 @@ import os
 import torch
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
-if os.environ.get("TORCH_CUDA_ARCH_LIST"):
-    # Let PyTorch builder to choose device to target for.
-    device_capability = ""
-else:
-    device_capability = torch.cuda.get_device_capability()
-    device_capability = f"{device_capability[0]}{device_capability[1]}"
+
+SUPPORTED_CUDA_ARCHS = {"80", "90"}
+
 
 nvcc_flags = [
     "--ptxas-options=-v",
     "--optimize=2",
 ]
-if device_capability:
+for device_capability in SUPPORTED_CUDA_ARCHS:
     nvcc_flags.append(
         f"--generate-code=arch=compute_{device_capability},code=sm_{device_capability}"
     )
